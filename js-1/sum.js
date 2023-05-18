ebe61b4d8a7d5f00d369adc1e92498c4fcfedf30
Danielfebamo <bilin241@hotmail.com>
2023-05-17 23:35:50 -0500

    solucion a ejercicios js

---
+++ js-1/sum.js
@@ -0,0 +1,12 @@
+function suma (num){
+
+    var total=0;
+    for (var i=1; i<= num;i++) {
+        total=total+i;       
+            }
+            return total;
+
+}
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
